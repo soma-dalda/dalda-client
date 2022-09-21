b89e78a0675e831c718d3c1786288a45886ee195
jaewoong2 <jwisgenius@naver.com>
2022-09-21 21:04:56 +0900

    :fire: api 서버 ssl 인증에 따른 코드 삭제

--- src/components/blocks/MetaTag.tsx
+++ src/components/blocks/MetaTag.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet-async'
 
 const MetaTag = () => {
-  return (
-    <Helmet>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-    </Helmet>
-  )
+  return <Helmet />
 }
 
 export default MetaTag
