ca6531b050f502edab59ba8377daeea0acf94a16
jaewoong2 <jwisgenius@naver.com>
2022-10-25 22:20:59 +0900

    pacakge: dayjs 라이브러리 추가 (#71)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+
+dayjs.locale('ko')
+
 export function getCookie(name: string): string | null {
   const nameLenPlus = name.length + 1
   return (
@@ -30,4 +34,21 @@ export const getDateTime = (date: Date) => {
     .toISOString()
     .split('.')[0]
     .substring(0, 16)
+}
+export function timeForToday(value: Date | string) {
+  const today = new Date()
+  const timeValue = new Date(value)
+
+  const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60)
+  if (betweenTime < 1) return '방금전'
+  if (betweenTime < 60) {
+    return `${betweenTime}분전`
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60)
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간전`
+  }
+
+  return dayjs(value).format('YY.MM/DD')
 }
